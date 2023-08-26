ad3eb0adc2a47ec1d966ba482b1ebf81a6c906ca
walkerhsu <walkerhsu0808@gmail.com>
2023-08-26 22:38:40 +0800

    modify jsut_script

--- TTS-systems/scripts/jsut_hts2textgrid.py
+++ TTS-systems/scripts/jsut_hts2textgrid.py
@@ -4,8 +4,8 @@ from tqdm import tqdm
 from nnmnkwii.io import hts
 
 
-label_dir = "/work/u7663915/Data/jsut-lab"  # change to local path
-output_dir = "/work/u7663915/TTS-systems/preprocessed_data/JSUT/TextGrid/jsut"  # change to local path
+label_dir = "./raw_data/jsut-lab"  # change to local path
+output_dir = "./preprocessed_data/JSUT/TextGrid/jsut"  # change to local path
 os.makedirs(output_dir, exist_ok=True)
 for d in os.listdir(label_dir):
     if not os.path.isdir(f"{label_dir}/{d}") or d[0] == '.':
